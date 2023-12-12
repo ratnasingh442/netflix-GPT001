08f3204e2ec45f8e7afeee1382f55b82e8431ee7
Ratna Singh <ratnasingh442@gmail.com>
2023-12-13 01:39:28 +0530

    adding form validation

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,8 +1,22 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { NETFLIX_BACKGROUND_IMG } from './utils/constants'
+import { checkValidation } from './utils/validate';
 
 const Browse = () => {
   const [isSignIn, setIsSignIn] = useState(true);
+  const email = useRef();
+  const password = useRef();
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  const handleBtnClick = () =>{
+
+    const message = checkValidation(email.current.value, password.current.value);
+
+    console.log(message);
+    setErrorMsg(message);
+
+
+  }
 
   const handleToggleSignIn = () =>{
     setIsSignIn(!isSignIn);
@@ -13,7 +27,7 @@ const Browse = () => {
       < img src={NETFLIX_BACKGROUND_IMG} alt="background-image"/>
     </div>
     <div className='bg-black absolute z-20 mx-auto right-0 left-0 my-36 h-6/12 w-3/12 rounded-lg bg-opacity-80'>
-<form className='text-white p-10' >
+<form className='text-white p-10' onSubmit={(e)=>e.preventDefault()} >
 
 
   <h1 className='font-bold text-3xl mx-auto left-0 right-0 w-15 block '>{isSignIn? 'Sign In': 'Sign Up'}</h1>
@@ -22,14 +36,15 @@ const Browse = () => {
       <input type="text" placeholder='Full Name' className='p-4 mx-auto left-0 right-0 my-8 w-full block bg-gray-700 rounded-lg' />
     )
   }
-  <input type="email" placeholder='Email Address' className='p-4 mx-auto left-0 right-0 my-8 w-full block bg-gray-700 rounded-lg' />
-  <input type="password" placeholder='Password' className='p-4 mx-auto left-0 right-0 w-full block bg-gray-700 rounded-lg'/>
+  <input type="email" placeholder='Email Address' className='p-4 mx-auto left-0 right-0 my-8 w-full block bg-gray-700 rounded-lg' ref={email} />
+  <input type="password" placeholder='Password' className='p-4 mx-auto left-0 right-0 w-full block bg-gray-700 rounded-lg' ref={password}/>
   {
     !isSignIn && (
       <input type="password" placeholder='Confirm Password' className='p-4 mx-auto left-0 right-0 my-8 w-full block bg-gray-700 rounded-lg' />
     )
   }
-  <button className='bg-red-500 px-20 py-4 mx-auto left-0 right-0 my-8 w-full block rounded-lg'>{isSignIn? 'Sign In': 'Sign Up'}</button>
+  <p className='text-red-500'>{errorMsg}</p>
+  <button className='bg-red-500 px-20 py-4 mx-auto left-0 right-0 my-8 w-full block rounded-lg' onClick={handleBtnClick}>{isSignIn? 'Sign In': 'Sign Up'}</button>
   <h4 className='cursor-pointer' onClick={handleToggleSignIn}>{isSignIn?'New to Netflix? Sign Up!': 'Already have account? Sign In'}</h4>
 </form>
     </div>
---
+++ src/components/utils/validate.js
@@ -0,0 +1,13 @@
+export const checkValidation  = (email, password) =>{
+
+    const isValidEmail = /^(?:(?!.*?[.]{2})[a-zA-Z0-9](?:[a-zA-Z0-9.+!%-]{1,64}|)|\"[a-zA-Z0-9.+!% -]{1,64}\")@[a-zA-Z0-9][a-zA-Z0-9.-]+(.[a-z]{2,}|.[0-9]{1,})$/.test(email);
+    const isValidPass = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(password);
+
+    if(!isValidEmail)return "Email Id not valid!";
+    if(!isValidPass)return "Password not valid";
+
+    return null;
+
+
+
+}
